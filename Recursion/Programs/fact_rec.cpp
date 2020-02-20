e86b476e8a83973e9bf15fdbc496628ac5a57033
M1NH42 <minhazalam365@gmail.com>
2020-02-20 19:02:31 +0530

    Factorial using recurssive function

---
+++ Recursion/Programs/fact_rec.cpp
@@ -0,0 +1,31 @@
+// factorial cal using recursion
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int factorial(int n)
+{
+    if(n<0)
+    {
+        cout <<"Enter a poositive number\n";
+        return 0;
+    }
+    else if(n==0)
+    {
+        return 1;
+    }
+    else
+    {
+        return factorial(n-1)*n;
+    }
+}
+int main()
+{
+    int num;
+    cout<<"Enter a number\n";
+    cin>>num;
+
+    cout<<"Factorial of a given number is : " << factorial(num)<<endl;
+    return 0;
+}
