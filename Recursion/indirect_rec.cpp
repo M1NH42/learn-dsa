29bec81de86623ce007c6da0dc9e2bcb9d094eb6
M1NH42 <minhazalam365@gmail.com>
2020-02-20 16:18:36 +0530

    Tree recursion program added

---
+++ Recursion/indirect_rec.cpp
@@ -0,0 +1,34 @@
+// programs shows the working of indirect recursion
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void fun_2(int n);
+
+void fun_1(int n)
+{
+    if(n>0)
+    {
+        cout<<n << " ";
+        fun_2(n-1);
+    }
+}
+
+void fun_2(int n)
+{
+    if(n>1)
+    {
+        cout<<n<<" ";
+        fun_1(n/2);
+    }
+    //cout<<"\n";
+}
+
+int main()
+{
+    int a;
+    cin>>a;
+    fun_1(a);
+
+    return 0;
+}
