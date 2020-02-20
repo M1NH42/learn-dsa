f1c31f88e008a9d28db3de59363cc9751eda125a
M1NH42 <minhazalam365@gmail.com>
2020-02-20 16:42:21 +0530

    Recursive function to return sum of n natural numbers

---
+++ Recursion/Programs/sum_n.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+// takes O(n) time complexity
+int sum_n(int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    else
+    {
+        return sum_n(n-1)+n;
+    }
+    
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    cout<<sum_n(n);
+
+    return 0;
+}
