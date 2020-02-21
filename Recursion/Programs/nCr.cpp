fdfe8268960759f31cffdc838dc1a9ce60aad10a
M1NH42 <minhazalam365@gmail.com>
2020-02-21 16:47:40 +0530

    [Added nCr function using recurssion]

---
+++ Recursion/Programs/nCr.cpp
@@ -0,0 +1,19 @@
+// this program finds out the nCr
+// if given with n and r as shown:
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, r;
+    cout<<"Enter n: ";
+    cin>>n;
+
+    cout<<"Enter r: ";
+    cin>>r;
+
+    cout<<nCr(n,r);;
+
+    return 0;
+}
