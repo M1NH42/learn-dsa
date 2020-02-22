c56058cfcac50401d5930d2572f9997bf893edf0
M1NH42 <minhazalam365@gmail.com>
2020-02-23 00:43:23 +0530

    [Added] various operation in array & comments for clarity

--- Array_ADT/arra_adt.cpp
+++ Array_ADT/arra_adt.cpp
@@ -1,3 +1,50 @@
 // in this program we'll code and learn about array ADT
 // how they declared and various operations performed
 // on the
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Array // needed to combine properties of an array in one
+{
+    int *A;
+    int size;
+    int length;
+};
+
+void display(struct Array arr)
+{
+    cout<<"Elements of an array\n";
+    for(int i=0;i<arr.length;i++)
+    {
+        cout<<arr.A[i]<<endl; // prints the elememts of an array
+    }
+}
+
+int main()
+{      
+    struct Array arr;
+
+    cout<<"Size of an array: ";
+    cin>>arr.size;
+
+    arr.A =(int *)malloc(arr.size*sizeof(int)); // dynamically allocated heap memory to A
+
+    int n;
+    cout<<"Enter lenth of an arraY: ";
+    cin>>n;
+
+    arr.length=0;
+
+    cout<<"Elements of an array\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr.A[i]; //takes input from user upto n-1 elements
+    }
+
+    arr.length=n; // set length equals n
+
+    display(arr);
+
+    return 0;
+}
