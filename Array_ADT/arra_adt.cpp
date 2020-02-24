c751f8c0ff338032d0b9762da912d4a83b89f591
M1NH42 <minhazalam365@gmail.com>
2020-02-24 22:17:32 +0530

    [Added] comments for clarity

--- Array_ADT/arra_adt.cpp
+++ Array_ADT/arra_adt.cpp
@@ -13,7 +13,7 @@ struct Array // needed to combine properties of an array in one
 };
 
 void append(struct Array *arr, int x) // this fuction will first chech if lenght<size only then
-{
+{                                     // insert element x at the ed of the array
     if(arr->length < arr->size)
     {
         arr->A[arr->length]=x;
