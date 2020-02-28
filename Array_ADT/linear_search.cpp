f9017b9ba39ad0cef78b068c1e78fb8b92a55f80
M1NH42 <minhazalam365@gmail.com>
2020-02-29 00:03:03 +0530

    added display()

--- Array_ADT/linear_search.cpp
+++ Array_ADT/linear_search.cpp
@@ -1,1 +1,20 @@
 // we'll be performinglinear search in an array to return the index of the given element
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Array // needed to combine properties of an array in one
+{
+    int *A;
+    int size;
+    int length;
+};
+
+void display(struct Array arr)
+{
+    cout<<"Elements of an array\n";
+    for(int i=0;i<arr.length;i++)
+    {
+        cout<<arr.A[i]<<endl; // prints the elememts of an array
+    }
+}
