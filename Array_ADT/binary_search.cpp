7418846002994797573d6397c2876112d92b5781
M1NH42 <minhazalam365@gmail.com>
2020-02-28 23:31:52 +0530

    [Added] binary search using array adt

--- Array_ADT/binary_search.cpp
+++ Array_ADT/binary_search.cpp
@@ -1,1 +1,11 @@
 // we'll be performing binary search on a sorted array
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Array // needed to combine properties of an array in one
+{
+    int *A;
+    int size;
+    int length;
+};
