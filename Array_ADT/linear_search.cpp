81fde386029812195cf67b064153cb4fc56d3516
M1NH42 <minhazalam365@gmail.com>
2020-02-29 00:04:15 +0530

    Some code

--- Array_ADT/linear_search.cpp
+++ Array_ADT/linear_search.cpp
@@ -18,3 +18,46 @@ void display(struct Array arr)
         cout<<arr.A[i]<<endl; // prints the elememts of an array
     }
 }
+
+int main()
+{      
+    struct Array arr;
+
+    cout<<"Size of an array: ";
+    cin>>arr.size;
+
+    arr.A =(int *)malloc(arr.size*sizeof(int)); // dynamically allocated heap memory to A
+
+    int n;
+    cout<<"Enter lenth of an arraY: ";
+    cin>>n;
+
+    arr.length=0;
+
+    cout<<"Elements of an array\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr.A[i]; //takes input from user upto n-1 elements
+    }
+
+    arr.length=n; // set length equals n
+
+    /* cout<<"Enter x: ";
+    int x;
+    cin>>x;
+ */
+    //append(&arr,x);
+
+    cout<<"Enter index: ";
+    int index;
+    cin>>index;
+    //insert_at(&arr,index,x);
+
+    delete_arr(&arr, index);
+
+    display(arr);
+
+    cout<<"Length of the array: "<<arr.length<<endl;
+
+    return 0;
+}
