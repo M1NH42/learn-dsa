0ea4e9c54fb30dae139ab71116fbf25f428982df
M1NH42 <minhazalam365@gmail.com>
2020-02-29 18:21:57 +0530

    coding!

--- Array_ADT/binary_search.cpp
+++ Array_ADT/binary_search.cpp
@@ -1,11 +1,51 @@
 // we'll be performing binary search on a sorted array
+// first we'll perfrom iterative binary search and after that
+// we'll see how recurssive binary search works
+// and also difference between these two methods
 
 #include<bits/stdc++.h>
 using namespace std;
 
-struct Array // needed to combine properties of an array in one
+/* void display()
 {
-    int *A;
-    int size;
-    int length;
-};
+
+} */
+
+int i_binary_search(int p, int l, int h, int key)
+{
+    int l, h, mid;
+    l=0;
+    h=n-1;
+    while(l<=h)
+    {
+        mid= (l+h)/2;
+
+        if(key==p[mid])
+        {
+            return mid;
+        }
+        else if(key<p[mid])
+        {
+            
+        }
+    }
+}
+
+int main()
+{
+
+    static int n;
+    cout<<"Enter n: ";
+    cin>>n;
+
+    int *p;
+    p= (int *)malloc(n* sizeof(int));
+
+    // insert elments in the heap array
+    for(int i=0; i<n;i++)
+    {
+        cin>>p[i];
+    }
+
+
+}
