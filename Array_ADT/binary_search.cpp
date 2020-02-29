f63bca7342d0ed184caafa4a6eda6ad7ef3bc20f
M1NH42 <minhazalam365@gmail.com>
2020-02-29 18:51:25 +0530

    Added binary search program

--- Array_ADT/binary_search.cpp
+++ Array_ADT/binary_search.cpp
@@ -12,3 +12,55 @@ struct Array // needed to combine properties of an array in one
     int size;
     int length;
 };
+
+void display(struct Array arr)
+{
+    cout<<"Elements of an array\n";
+    for(int i=0;i<arr.length;i++)
+    {
+        cout<<arr.A[i]<<endl; // prints the elememts of an array
+    }
+}
+
+int main()
+{      
+    struct Array arr;
+
+    cout<<"Size of an array: ";
+    cin>>arr.size;
+
+    arr.A =(int *)malloc(arr.size*sizeof(int)); // dynamically allocated heap memory to A
+
+    int n;
+    cout<<"Enter lenth of an arraY: ";
+    cin>>n;
+
+    //arr.length=0;
+
+    cout<<"Elements of an array\n";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr.A[i]; //takes input from user upto n-1 elements
+    }
+
+    //arr.length=n; // set length equals n
+
+    /* cout<<"Enter x: ";
+    int x;
+    cin>>x;
+ */
+    //append(&arr,x);
+
+    /* cout<<"Enter index: ";
+    int index;
+    cin>>index; */
+    //insert_at(&arr,index,x);
+
+    //delete_arr(&arr, index);
+
+    display(arr);
+
+    cout<<"Length of the array: "<<arr.length<<endl;
+
+    return 0;
+}
