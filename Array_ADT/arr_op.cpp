920f85db10027dc19e52e2d001dce6a4d6cc1aa9
M1NH42 <minhazalam365@gmail.com>
2020-03-02 00:11:47 +0530

    Added insert in the sorted array

--- Array_ADT/arr_op.cpp
+++ Array_ADT/arr_op.cpp
@@ -11,10 +11,21 @@ using namespace std;
 // displays array elements from index 0 to n-1
 void display(int arr[], int n)
 {
-    for(int i=0; i<n; i++)
+    for(int i=0; i<=n; i++)
     {
         cout<<arr[i]<<endl;
     }
+}
+
+void insert_in_sorted(int arr[],int n, int x)
+{
+    int i=n-1;
+    while(arr[i]>x) // check if the last array element is greater than the x
+    {
+        arr[i+1]=arr[i];
+        i--;
+    }
+    arr[i+1]=x;
 }
 
 int main()
@@ -29,6 +40,14 @@ int main()
     {
         cin>>a[i]; //stores in array of size n-1
     }
+
+    int val;
+    cout<<"Enter val: ";
+    cin>>val;
+
+    insert_in_sorted(a,n,val);
+
+    display(a,n);
 
     return 0;
 }
