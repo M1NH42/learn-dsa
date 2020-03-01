55e79400457525ac12a197cf1be952998234d1ae
M1NH42 <minhazalam365@gmail.com>
2020-03-02 00:01:17 +0530

    Perforn array operations such as sort, insert in sorted array and arrang -ve and +ve numbers

---
+++ Array_ADT/arr_op.cpp
@@ -0,0 +1,14 @@
+// this program performs few operation on an array
+/*  Operations are:
+    * insert lement in a sorted array
+    * check if array is sorted or not
+    * bring -ve on left and +ve on right
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
