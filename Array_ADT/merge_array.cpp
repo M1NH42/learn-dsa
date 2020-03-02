973fedbe76e80ee4bebdecaf537904ade562951d
M1NH42 <minhazalam365@gmail.com>
2020-03-03 01:04:07 +0530

    Bazinga!

--- Array_ADT/merge_array.cpp
+++ Array_ADT/merge_array.cpp
@@ -4,12 +4,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// displays the array elements
 void display(int a[], int n)
 {
     for(int i=0; i<n; i++)
     {
         cout<<a[i]<<endl; // prints array elements
     }
+}
+
+int merge_arrays(int a[], int n, int b[], int m)
+{
+    
 }
 
 int main()
@@ -23,13 +29,17 @@ int main()
 
     int A[n], B[m];
 
+    cout<<"*********************************************\n";
     // take input in array A[n]
+    cout<<"Enter elements in sorted order\n";
     for(int i=0; i<n; i++)
     {
         cin>>A[i]; // stores value in array index starting from 0 to n
     }
 
+    cout<<"*********************************************\n";
     // take input in array B[m]
+    cout<<"Enter elements in sorted order\n";
     for(int i=0; i<m; i++)
     {
         cin>>B[i]; // stores elements in B[] index starting from 0 t0 m-1
