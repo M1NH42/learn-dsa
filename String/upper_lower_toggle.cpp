b91ec19a5430e986e02c09c3406ebf435b5148f5
M1NH42 <minhazalam365@gmail.com>
2020-03-04 18:14:19 +0530

    Toggle upper and lower cases in string

--- String/upper_lower_toggle.cpp
+++ String/upper_lower_toggle.cpp
@@ -2,4 +2,44 @@
 // by adding difference of upper case to lower case
 // A=65 & a=97 , diff=32
 // A+32=65+32 = 97 = a // for upper to lower
-// a-32=97-32=65= A // for lower to upper 
+// a-32=97-32=65= A // for lower to upper
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter n : ";
+    cin>>n;
+
+    char a[n];
+
+    // take input
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i]; // stores input
+    }
+
+    // show output
+    for(int i=0;i<n;i++)
+    {
+        cout<<a[i];
+    }
+    cout<<endl;
+
+    for(int i=0;a[i]!='\0';i++)
+    {
+        if(a[i]>= 'A' && a[i]<= 'Z')
+        {
+            a[i] += 32;
+        }
+        else if(a[i]>='a' && a[i]<='z')
+        {
+            a[i] -= 32;
+        }
+    }
+    printf("%s",a);
+
+    return 0;
+}
