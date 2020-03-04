54f5e464b0c2ec7b73287dccd477872ec0da7a9e
M1NH42 <minhazalam365@gmail.com>
2020-03-04 18:07:36 +0530

    Defining the program

---
+++ String/upper_lower_toggle.cpp
@@ -0,0 +1,5 @@
+// This program toggles between upper case and lower case
+// by adding difference of upper case to lower case
+// A=65 & a=97 , diff=32
+// A+32=65+32 = 97 = a // for upper to lower
+// a-32=97-32=65= A // for lower to upper 
