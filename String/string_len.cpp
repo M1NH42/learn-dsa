2d79c99b80d9c89e3a9e6ebd4f78847cb41be881
M1NH42 <minhazalam365@gmail.com>
2020-03-04 17:50:26 +0530

    Program to find length of a string

---
+++ String/string_len.cpp
@@ -0,0 +1,20 @@
+// in this program we'll find the length of a string given
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char *str = "Minhaz";
+    /* cout<<"ENter a string: ";
+    gets(&str); */
+
+    int i;
+    for(i=0; str[i] != '\0'; i++)
+    {
+
+    }
+    cout<<"String length : "<<i<<endl;
+
+    return 0;
+}
