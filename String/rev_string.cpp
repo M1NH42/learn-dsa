b3bc1a4a2d1d5b7736042352a7ff4d55d428a3bf
M1NH42 <minhazalam365@gmail.com>
2020-03-05 00:21:06 +0530

    Formatted for better readability

--- String/rev_string.cpp
+++ String/rev_string.cpp
@@ -3,7 +3,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     int n; // size of char array if n=5 then 4 elements because it includes a null character
     cout<<"Enter n: ";
     cin>>n;
@@ -11,17 +12,20 @@ int main(){
     char a[n];
 
     // take input from keyboard/user
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n;i++)
+    {
         cin>>a[i];
     }
     int j;
-    for(j=0;a[j]!='\0'; j++){
+    for(j=0;a[j]!='\0'; j++)
+    {
         // this will make j represent the end of the string i.e char end
     }
     j -= 1;
 
     char t;
-    for(int i=0; i<j; i++,j--){ // we'll swap the elements from i=0 with j=n-1
+    for(int i=0; i<j; i++,j--)
+    { // we'll swap the elements from i=0 with j=n-1
         t=a[i];
         a[i]=a[j];
         a[j]=t;
