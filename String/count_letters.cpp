9cb4a6ca031b885d4b79c79fcade08d837feaa7d
M1NH42 <minhazalam365@gmail.com>
2020-03-04 18:42:08 +0530

    Comments for clarity

--- String/count_letters.cpp
+++ String/count_letters.cpp
@@ -10,12 +10,12 @@ int main()
     int v_count=0, c_count=0;
 
     for(int i=0; a[i]!='\0'; i++)
-    {
+    {   // checks if 'a,e,i,o,u' both in upper as well as lower cases are present or not if pesent then increase the count by 1 and keep traversing and increasing the counter
         if(a[i]=='a' || a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
         {
             v_count++;
         }
-        else if((a[i]>='A' && a[i]<='Z') || (a[i]>='a' && a[i]<='z'))
+        else if((a[i]>='A' && a[i]<='Z') || (a[i]>='a' && a[i]<='z')) // checks if present in the alphabet range or not
         {
             c_count++;
         }
