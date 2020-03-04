ca5fc63f5e45d598e64f0081dbcc4bb7dfaa751b
M1NH42 <minhazalam365@gmail.com>
2020-03-04 17:51:58 +0530

    Comments for clarity

--- String/string_len.cpp
+++ String/string_len.cpp
@@ -5,15 +5,17 @@ using namespace std;
 
 int main()
 {
+    // string declaration
     char *str = "Minhaz";
     /* cout<<"ENter a string: ";
     gets(&str); */
 
     int i;
-    for(i=0; str[i] != '\0'; i++)
+    for(i=0; str[i] != '\0'; i++) // it traverse the char array uptill '\0'
     {
 
     }
+    // here we'll get value of i at the null char which is the length of the string
     cout<<"String length : "<<i<<endl;
 
     return 0;
