770ff7b03a3fb5a108a94b61ed114890f3db8d67
M1NH42 <minhazalam365@gmail.com>
2020-03-04 18:39:55 +0530

    Working Program Added

--- String/count_letters.cpp
+++ String/count_letters.cpp
@@ -1,2 +1,28 @@
 // In this program we'll count the number of vowels and consonents
 // as shown below with explanation in comments section
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char a[]="How are you";
+    int v_count=0, c_count=0;
+
+    for(int i=0; a[i]!='\0'; i++)
+    {
+        if(a[i]=='a' || a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='U')
+        {
+            v_count++;
+        }
+        else if((a[i]>='A' && a[i]<='Z') || (a[i]>='a' && a[i]<='z'))
+        {
+            c_count++;
+        }
+    }
+    cout<<"Number of vowels: "<<v_count<<endl;
+
+    cout<<"Number of consonents: "<<c_count<<endl;
+
+    return 0;
+}
