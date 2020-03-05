aa93c8b814ca7dab475d5ef2ed7e3f0f720ae459
M1NH42 <minhazalam365@gmail.com>
2020-03-06 00:06:52 +0530

    Added working program to find if string contain duplicate elements or not

--- String/duplicate_detect.cpp
+++ String/duplicate_detect.cpp
@@ -4,3 +4,61 @@
     * And then check if any of the element occurs more 
     * than once then return duplicate else not duplicate
 */
+
+#include<bits/stdc++.h>
+using namespace std;
+/* 
+int duplicate(int a[], int n)
+{
+    int h[26]={0}; // create hash table of size 26 (0-25) letters
+
+    for(int i=0; a[i] != '\0'; i++)
+    {
+        h[a[i]-97] += 1;
+    }
+    for(int i=0;i<26;i++)
+    {
+        if(h[i]>1)
+        {
+            cout<<"Duplicate elements\n";
+            //return 0;
+        }
+        else
+        {
+            cout<<"Unique elements\n";
+            return 0;
+        }   
+    }
+}
+ */
+int main()
+{
+    /* int n;
+    cout<<"Enter n: ";
+    cin>>n;
+
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i]; // takes input from the user
+    } */
+    /////duplicate(a,n);
+    char a[]="Minhaz";
+    int h[26]={0}; // create hash table of size 26 (0-25) letters
+
+    for(int i=0; a[i] != '\0'; i++)
+    {
+        h[(a[i]-97)] += 1;
+    }
+    for(int i=0;i<26;i++)
+    {
+        if(h[i]>1)
+        {
+            cout<<"Duplicate elements\n";
+            //return 0;
+        }
+    }
+
+    cout<<"Unique elements\n";
+    return 0;
+}
