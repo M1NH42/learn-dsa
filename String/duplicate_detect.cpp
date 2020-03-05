ea9ad335edf8f60bbd5975ed56ebeaa01995361b
M1NH42 <minhazalam365@gmail.com>
2020-03-05 23:40:20 +0530

    Program description

---
+++ String/duplicate_detect.cpp
@@ -0,0 +1,6 @@
+/* 
+    * This program finds duplicate letter from the srting
+    * Here we'll use hash table to store their count
+    * And then check if any of the element occurs more 
+    * than once then return duplicate else not duplicate
+*/
