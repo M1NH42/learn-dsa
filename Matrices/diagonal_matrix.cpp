24cfdd5bc509130955a5a50fa9c0a0247fa1c256
M1NH42 <minhazalam365@gmail.com>
2020-03-07 12:18:19 +0530

    Added basic structure of n array

---
+++ Matrices/diagonal_matrix.cpp
@@ -0,0 +1,12 @@
+/*  * In this program we'll be working with diagonal matrix in which only the
+    * diagonal elements are non-zero and rest are all zero
+    * we'll be perfroming some supporting functions such as get and set
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
