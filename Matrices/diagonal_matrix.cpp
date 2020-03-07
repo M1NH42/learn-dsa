739d00cf59d6f648e16412de4dc0430b2780bc5b
M1NH42 <minhazalam365@gmail.com>
2020-03-07 12:37:07 +0530

    Added some supporting functions

--- Matrices/diagonal_matrix.cpp
+++ Matrices/diagonal_matrix.cpp
@@ -6,7 +6,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct Matrix
+{
+    int A[10];
+    int n;
+};
+
+void Set(Matrix *m, int i, int j, int x)
+{
+    if(i==j)
+    {
+        m->A[i-1]=x; // stores value iff i==j
+    }
+}
+
+int Get(Matrix m, int i, int j)
+{
+    if(i==j)
+    {
+        return m.A[i-1];
+    }
+    else
+    {
+        return 0;
+    }    
+}
+
 int main()
 {
+    struct Matrix m;
+
     return 0;
 }
