e6013bf5e03d179a6d6650dd2a83fcede83f6046
M1NH42 <minhazalam365@gmail.com>
2020-03-08 01:57:22 +0530

    Added workingprogram for diagonal matrix

--- Matrices/cpp_class_diag.cpp
+++ Matrices/cpp_class_diag.cpp
@@ -4,6 +4,51 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Diagonal{
+    private:
+        int n;
+        int *A;
+    public:
+        Diagonal(int n){
+            this->n = n;
+            A = new int[n];
+        }
+        void Set(int i, int j, int x);
+        int Get(int i, int j);
+        void display();
+        ~Diagonal(){
+            delete []A;
+        }
+};
+
+void Diagonal :: Set(int i, int j, int x){
+    if(i==j){
+        A[i-1] = x;
+    }
+}
+int Diagonal :: Get(int i, int j){
+    if(i==j){
+        return A[i-1];
+    }
+    else{
+        return 0;
+    }
+}
+
+void Diagonal :: display(){
+    for(int i=0;i<n;i++){
+        for(int j=0; j<n; j++){
+            if(i==j){
+                cout<<A[i]<<" ";
+            }
+            else{
+                cout<<"0 ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
 int main(){
     return 0;
 }
