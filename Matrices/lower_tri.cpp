a3c23b29526a13114c6882cbce2b9ee4c20ea6af
M1NH42 <minhazalam365@gmail.com>
2020-03-08 03:14:50 +0530

    Comments for clarity

---
+++ Matrices/lower_tri.cpp
@@ -0,0 +1,10 @@
+// This program implements the lower triangular matrix uisng row major
+// which means lements will be stored  row wise as shown: 
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    return 0;
+}
