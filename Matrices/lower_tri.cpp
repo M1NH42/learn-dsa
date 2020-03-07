e0879af3591864f868b7f2ddce0629e3a08f6823
M1NH42 <minhazalam365@gmail.com>
2020-03-08 03:21:44 +0530

    Added various matrix functions

--- Matrices/lower_tri.cpp
+++ Matrices/lower_tri.cpp
@@ -4,7 +4,53 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+struct Matrix
+{
+    int *A;
+    int n;
+};
+
+void Set(struct Matrix *m, int i, int j, int x)
+{
+    if(i>=j)
+    {
+        m->A[(i*(i-1))/2 + (j-1)] = x;
+    }
+}
+
+int Get(struct Matrix m, int i, int j)
+{
+    if(i>=j)
+    {
+        return m.A[i*(i-1)/2 + (j-1)];
+    }
+    else
+    {
+        return 0;
+    }
+    
+}
+
 int main()
 {
+    Matrix m;
+    /* m.A = (int *)malloc((m.n*m.n+1)/2* sizeof(int)); */
+
+    cout<<"Enter dimension: ";
+    cin>>m.n;
+
+    int x;
+
+    m.A = (int *)malloc((m.n*m.n+1)/2* sizeof(int));
+
+    for(int i=1;i<=m.n;i++)
+    {
+        for(int j=1;j<=m.n;j++)
+        {
+            cin>>x;
+            Set(&m, i, j, x);
+        }
+    }
+
     return 0;
 }
