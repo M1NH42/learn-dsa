6db8e3857c9b25d9c6ece322d0f92031805a1f55
M1NH42 <minhazalam365@gmail.com>
2020-03-19 15:06:11 +0530

    Menu driven programs for matrix using switch case

---
+++ Matrices/menu_driven.cpp
@@ -0,0 +1,23 @@
+/*
+    This is menu driven program for matrix operations
+    * create
+    * get()
+    * set()
+    * display() 
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int *A, dim, choice;
+
+    // message to enter dimension
+    cout<<"Enter dimensions\n";
+    cin>>dim;
+
+    A = (int *)malloc(dim * sizeof(int));
+
+    
+}
