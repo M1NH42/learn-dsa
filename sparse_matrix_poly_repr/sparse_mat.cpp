a6699ae67cac2335caecb04d4660cee19cec09f1
M1NH42 <minhazalam365@gmail.com>
2020-04-24 12:37:26 +0530

    Add program description

---
+++ sparse_matrix_poly_repr/sparse_mat.cpp
@@ -0,0 +1,6 @@
+// Intro : In thi program we will see the implementation of the sparse matrix
+// which is nothing but a matrix with lots of zeroes
+// canbe represented by following two methods
+// * 3 column representation
+// * compressed sparse matrix
+
