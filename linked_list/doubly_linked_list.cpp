a6ec582aeb1f30022bfaf924c3070e59f792fe06
M1NH42 <minhazalam365@gmail.com>
2020-05-12 16:09:55 +0530

    Add structure for doubly linked list

--- linked_list/doubly_linked_list.cpp
+++ linked_list/doubly_linked_list.cpp
@@ -1,2 +1,17 @@
 /* In this program we'limplement doubly linked list
 with the help of which we can move in either direction */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    struct Node *prev;
+    int data;
+    struct Node *next;
+}*first=NULL;
+
+int main()
+{
+    return 0;
+}
