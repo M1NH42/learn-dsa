c2792b97b6c2a836913baccf222798a2e6d99ebf
M1NH42 <minhazalam365@gmail.com>
2020-05-13 13:17:52 +0530

    Add stack operation push

---
+++ Stack/stack_using_array.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// typedef
+// typedef rep(i,a,b) for(int i=a, i<b; i++)
+
+struct Stack
+{
+    int size;
+    int top;
+    int *S;
+};
+
+void create_stack(struct Stack *st)
+{
+    cout<<"Enter size\n";
+    cin>>st->size;
+
+    st->top=-1;
+
+    st->S=(int *)malloc(st->size * sizeof(int));
+}
+
+// display the stack
+void display(struct Stack st)
+{
+    for(int i=st.top; i>=0; i--)
+    {
+        cout<<st.S[i]<<"\n";
+    }
+    cout<<"\n";
+}
+
+/* *********** operations ************* */
+void push(struct Stack *st, int x)
+{
+    if (st->top == st->size-1)
+    {
+        cout<<"stack overflow!\n";
+    }
+    else
+    {
+        st->top++;
+        st->S[st->top]=x;
+    }
+}
+
+int main()
+{
+    struct Stack st;
+
+    create_stack(&st);
+
+    push(&st, 10);
+    push(&st, 20);
+    push(&st, 30);
+    push(&st, 40);
+
+    display(st);
+
+    return 0;
+}
