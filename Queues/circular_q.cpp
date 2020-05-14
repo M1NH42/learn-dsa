04ba3c39dfe257902f8d68d07fa7e7a7eddfd00c
M1NH42 <minhazalam365@gmail.com>
2020-05-14 13:33:17 +0530

    Add enq() and deq()

---
+++ Queues/circular_q.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define rep(i,a,b) for(int i=a;i<=b;i++)
+
+struct Queue
+{
+    int size;
+    int front;
+    int rear;
+    int *Q;
+};
+
+void create(struct Queue *q, int size)
+{
+    q->size=size;
+
+    q->front=q->rear=0;
+
+    q->Q = (int *)malloc(q->size * sizeof(int));
+}
+
+// enqueue in circular queue
+void enqueue(struct Queue *q, int x)
+{
+    // check full condition
+    if((q->rear+1)%q->size == q->front)
+    {
+        cout<<"q full hai\n";
+    }
+    else
+    {
+        q->rear = (q->rear+1) % q->size;
+        q->Q[q->rear] = x;
+    }   
+}
+
+int dequeue(struct Queue *q)
+{
+    int x =-1;
+
+    // check mepty condition
+    if(q->rear==q->front)
+    {
+        cout<<"empty hai\n";
+    }
+    else
+    {
+        q->front = (q->front+1) % q->size;
+        x = q->Q[q->front];
+    }
+    return x;    
+}
+
+int main()
+{
+    struct Queue q;
+
+    create(&q, 5);
+
+    return 0;
+}
