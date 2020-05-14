e6e6e5f602000a8bbeb40bebaa752fde0c0b20cd
M1NH42 <minhazalam365@gmail.com>
2020-05-14 12:34:36 +0530

    Add structure fr the queue

--- Queues/q_using_array.cpp
+++ Queues/q_using_array.cpp
@@ -1,2 +1,25 @@
 /* Add description: Queue in this program is implemented using array
    Queue follows FIFO (frist in first out) */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Queue
+{
+    int size;
+    int front;
+    int rear;
+    int *Q;
+};
+
+int main()
+{
+    struct Queue q;
+
+    cout<<"Enter size: \n";
+    cin>>q.size;
+
+    q.Q = (int *)malloc(q.size * sizeof(int));
+
+    q.front = q.rear = -1;
+}
