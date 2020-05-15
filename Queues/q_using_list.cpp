60792a5e9d04957315180b21548a07ea8a7efc4f
M1NH42 <minhazalam365@gmail.com>
2020-05-16 02:17:52 +0530

    Enqueue(int x)

--- Queues/q_using_list.cpp
+++ Queues/q_using_list.cpp
@@ -5,11 +5,39 @@ struct Node
 {
     int data;
     struct Node *next;
-};
+}*front=NULL, *rear=NULL;
 
-
+void enq(int x)
+{
+    // check full condition
+    Node *t = new Node;
+    if(t==NULL)
+    {
+        cout<<"Full hai\n";
+    }
+    else
+    {
+        t->data=x;
+        t->next=NULL;
+        
+        if(front==NULL)
+        {
+            front=rear=t;
+        }
+        else
+        {
+            rear->next=t;
+            rear=t;
+        }
+        
+    }
+    
+}
 
 int main()
 {
+    struct Node *q;
+    // q = (struct Node*) malloc(sizeof(struct Node));
+
     return 0;
 }
