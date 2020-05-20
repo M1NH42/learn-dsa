16ada3ba754223aaadf20bb53ab012e8a2b279c7
M1NH42 <minhazalam365@gmail.com>
2020-05-20 19:02:40 +0530

    Add delete_element() from max heap

--- Heap/heap.cpp
+++ Heap/heap.cpp
@@ -24,11 +24,50 @@ void create_heap(int H[], int n)
     }
 }
 
+int delete_element(int H[], int n)
+{
+    int x,temp;
+
+    int val=H[1];
+    H[n]=val;
+    x=H[n];
+    H[1]=H[n];
+    int i=1, j=2*i;//points to left child
+
+    while(j<n-1)
+    {
+        if(H[j+1]>H[j])
+        {
+            j=j+1;
+        }
+        if(H[i]<H[j])
+        {
+            temp=H[i];
+            H[i]=H[j];
+            H[j]=temp;
+            i=j;
+            j=2*j;
+        }
+        else
+        {
+            break;
+        }
+        
+    }
+    return val;
+}
+
 int main()
 {
     int H[]={0,10,20,30,25,5,40,35};
 
     create_heap(H,7);
+
+    // delete elements one-by-one
+    for(int i=7;i>0;i--)
+    {
+        cout<<"Deleted val : "<<delete_element(H,i)<<"\n";
+    }
 
     for(int i=1;i<=7;i++)
     {
