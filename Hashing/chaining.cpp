66c67727c10aa161005686c1f1d12020517e1935
M1NH42 <minhazalam365@gmail.com>
2020-05-21 13:38:40 +0530

    Add chaining hash

---
+++ Hashing/chaining.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#include "chains.h"
+
+int hash(int key)
+{
+    return key % 10;
+}
+
+void insert(struct Node * H[], int key)
+{
+    int index=hash(key);
+    sorted_insert(&H[index], key);
+}
+
+int main()
+{
+    struct Node *HT[10];
+    struct Node *temp;
+
+    for(int i=0; i<10; i++)
+    {
+        HT[i]=NULL;
+    }
+    insert(HT, 12);
+    insert(HT, 22);
+    insert(HT, 42);
+
+    temp=search(HT[hash(21)], 21);
+
+    cout<<temp->data;
+
+    return 0;
+}
