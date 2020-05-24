0935caed4c26d8e4c1d1d0344a6a8eccda833e11
M1NH42 <minhazalam365@gmail.com>
2020-05-24 14:44:41 +0530

    Add dfs()

--- Graph/bfs.cpp
+++ Graph/bfs.cpp
@@ -30,7 +30,19 @@ void BFS(int G[][7], int start, int n)
 
 void DFS(int G[][7], int start, int n)
 {
-
+    static int visited[7] = {0};
+    if(visited[start]==0)
+    {
+        cout<< start << endl;
+        visited[start]=1;
+        for(int j=1; j<=n; j++)
+        {
+            if(G[start][j]==1 && visited[j]==0)
+            {
+                DFS(G, j, n);
+            }
+        }
+    }
 }
 
 int main()
@@ -43,7 +55,7 @@ int main()
                    {0,0,0,0,1,0,0},
                    {0,0,0,0,1,0,0}};
 //    bfs called
-    BFS(G, 1, 7);
+    DFS(G, 1, 7);
 
     return 0;
 }
