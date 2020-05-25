c3676941c8d3c7ab95351bc2b269e48aa0b7aaee
M1NH42 <minhazalam365@gmail.com>
2020-05-25 19:22:27 +0530

    Add insert()

--- AVL/avl_insertion.cpp
+++ AVL/avl_insertion.cpp
@@ -7,3 +7,47 @@
    * nary search tree so in avl tree balancing factor
    * takes care of the height to minimum which results
    * in the search operation of the order (log(n)) */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    struct Node *lchild;
+    int data;
+    struct Node *rchild;
+} *root = NULL;
+
+struct Node * get_new_node(int data)
+{
+    struct Node * new_node;
+    new_node = (struct Node *)malloc(sizeof(struct Node));
+    new_node->data = data;
+    new_node->lchild = new_node->rchild = NULL;
+
+    return new_node;
+}
+
+struct Node * insert_in_avl(struct Node *p, int val)
+{
+    // check if there's any node
+    if(p == NULL)
+    {
+        p = get_new_node(val);
+    }
+    else if(val <= p->data)
+    {
+        p->lchild = insert_in_avl(p->lchild, val);
+    }
+    else
+    {
+        p->rchild = insert_in_avl(p->rchild, val);
+    }
+    return p;
+    
+}
+
+int main()
+{
+  return 0;
+}
