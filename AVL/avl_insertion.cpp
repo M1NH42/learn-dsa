2c426e801b793593ae0d463f1ecabcf0c12fdeea
M1NH42 <minhazalam365@gmail.com>
2020-05-25 19:08:15 +0530

    Add description about AVL tree

---
+++ AVL/avl_insertion.cpp
@@ -0,0 +1,9 @@
+/* In this program we'll be implementing AVL insertion and deletion */
+
+/* Intro: AVL is nothing but a self balancing binary search tree
+   * can also be called as perfect binary search tree
+   * Makes searchig much faster than the regular BST
+   * search operation depends on the height of the bi
+   * nary search tree so in avl tree balancing factor
+   * takes care of the height to minimum which results
+   * in the search operation of the order (log(n)) */
